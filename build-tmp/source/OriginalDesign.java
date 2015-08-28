ef98059c7862ecfa15fc6002e9aaa5756d31ddb9
annaz8 <zhong2721@att.net>
2015-08-28 08:31:27 -0700

    modified code

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,10 +1,26 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
+public void setup()
 { 
 	size(500, 300);
 
 }
 
-void draw()
+public void draw()
 {
   background(51);
   head();
@@ -14,7 +30,7 @@ void draw()
 }
 
 
-void head ()
+public void head ()
 {
 	fill(0,0,0);
 	noStroke();
@@ -22,7 +38,7 @@ void head ()
 	ellipse(310,150,30,30);
 }
 
-void body ()
+public void body ()
 {
 	fill(0,0,0);
 	rect(230,180,10,20);
@@ -36,7 +52,7 @@ void body ()
 	endShape();
 }
 
-void leg() 
+public void leg() 
 {
 	rect(270, 220,30,10 );
 	rect(280,220,10,80);
@@ -55,7 +71,7 @@ void leg()
 }
 
 int q;
-void light() 
+public void light() 
 {
 	fill(255,255,255,q);
 	beginShape();
@@ -71,3 +87,12 @@ void light()
 	}
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
